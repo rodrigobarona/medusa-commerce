211fa2b57612300a65e3c1a56c6d18733eb9c03f
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2025-02-24 11:32:23 -0800

    add customizatio to deply on vercel

--- sporting-store/medusa-config.ts
+++ sporting-store/medusa-config.ts
@@ -3,6 +3,11 @@ import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+  admin: {
+    disable: process.env.MEDUSA_DISABLE_ADMIN === "true" || false,
+    backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000",
+    path: `/${(process.env.MEDUSA_ADMIN_PATH || "app").replace(/^\/+/, "")}`,
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
@@ -13,5 +18,31 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
+    workerMode: process.env.MEDUSA_WORKER_MODE as
+      | "shared"
+      | "worker"
+      | "server",
   },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ],
 });
